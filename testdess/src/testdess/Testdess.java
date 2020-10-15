7d6d150f1f147de8950f5ab159d3acea3b3b7159
Abdesslem007 <yondaime4y@gmail.com>
2020-10-15 12:30:18 +0100

    zoudj el baboun

--- testdess/src/testdess/Testdess.java
+++ testdess/src/testdess/Testdess.java
@@ -17,6 +17,7 @@ public class Testdess {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hello world");
+        System.out.println("hello world");
     }
     
 }
